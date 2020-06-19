cdd38ceabf82602edef2016290e4fdf56ff99c07
Paul Latislaw <paullatislaw@gmail.com>
2020-06-19 18:11:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
  array.join("and")
 end
 
-if arr.length > 2
+# if arr.length > 2
 
-  this line help you to locate the and location arr[-1].insert(0, "and")
-  I use .join() and insert() methods
+  # this line help you to locate the and location arr[-1].insert(0, "and")
+  # I use .join() and insert() methods
