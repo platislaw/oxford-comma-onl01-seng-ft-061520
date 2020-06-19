712707406f6186e094d87348d0bd9e896d5e0fa8
Paul Latislaw <paullatislaw@gmail.com>
2020-06-19 18:10:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,9 @@
 def oxford_comma(array)
- array.join
+ array.join(" , ")
  array.join("and")
 end
+
+if arr.length > 2
+
+  this line help you to locate the and location arr[-1].insert(0, "and")
+  I use .join() and insert() methods
